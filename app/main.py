473ea25a16dd4bccd05f7ffe641448dcf413c129
satyajeetchandel07 <satyaprakashchandel14@gmail.com>
2026-08-18 13:55:42 +0530

    Update CORS middleware to allow new origin

--- app/main.py
+++ app/main.py
@@ -59,7 +59,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "https://ai-lms1.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
